a2f2e78863d2aea22189afcf05ba779bb40fc389
raethlo <roman.rostar@gmail.com>
2014-03-16 12:43:29 +0100

    Initial commit

---
+++ RushHour/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RushHour
+{
+    class Car
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
