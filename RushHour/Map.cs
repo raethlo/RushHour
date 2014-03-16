c90e841b9ad37f00b1bd832ac5b907c775a2dbe6
raethlo <roman.rostar@gmail.com>
2014-03-16 12:51:45 +0100

    Adds class for road situation

---
+++ RushHour/Map.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushHour
+{
+    class Map
+    {
+        //fields
+        List<Car> cars;
+        char[][] map;
+
+        public int Dimension { get; set; }
+        
+    }
+}
