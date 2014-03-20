5c08d1473c5263586af8fc303b46a6162e0accb2
raethlo <roman.rostar@gmail.com>
2014-03-20 19:06:38 +0100

    Adds BF solver method, State class now compolete

--- RushHour/Program.cs
+++ RushHour/Program.cs
@@ -8,76 +8,45 @@ namespace RushHour
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            TrafficGrid g = new TrafficGrid();
-            g.LoadGridFromFile("../../Resources/0.txt");
-            
-            foreach (var car in g.Cars)
-            {
-                Console.Write("Car {0}: ", car.Color);
-                switch (car.Orientation)
-                {
-                    case Orientation.Vertical:
-                        Console.WriteLine("\t can move {0} to {1}", g.CanMove(car, Direction.Down), Direction.Down.ToString());
-                        Console.WriteLine("\t can move {0} to {1}", g.CanMove(car, Direction.Up), Direction.Up.ToString());                                           
-                        break;
-                    case Orientation.Horizontal:
-                        Console.WriteLine("\t can move {0} to {1}",g.CanMove(car, Direction.Right),Direction.Right.ToString());
-                        Console.WriteLine("\t can move {0} to {1}", g.CanMove(car, Direction.Left), Direction.Left.ToString());
-                        try
-                        {
-                            Console.WriteLine("\t can move {0} to {1}", g.CanMove(car, Direction.Up), Direction.Up.ToString());
-                        }
-                        catch (Exception ex)
-                        {
-                            
-                            Console.WriteLine(ex.Message);
-                        }
-                        break;
-                    default:
-                        break;
-                }
-                 
-            }
+            //bool isSameParent = true;
+            State entry = new State();
+            entry.Traffic = new TrafficGrid();
 
-            System.Console.WriteLine("INITIAL!");
-            g.PrintGrid();
-            System.Console.WriteLine("___________________");
+            try 
+	        {	        
+		        entry.Traffic.LoadGridFromFile("../../Resources/0.txt");
+	        }
+	        catch (Exception ex)
+	        {
+		        System.Console.WriteLine(ex.Message);
+                return;
+		        //throw;
+	        }
 
-            foreach (TrafficGrid t in g.GeneratePossibleMoves())
-            {
-                System.Console.WriteLine();
-                t.PrintGrid();
-                System.Console.WriteLine();
-            }
+            entry.Parent = null;
 
-            //System.Console.WriteLine();
-            //System.Console.WriteLine("BEFORE \n");
-            //g.PrintGrid();
+            Solver solver= new Solver();
+            solver.BFSolve(entry);
 
-            //System.Console.WriteLine();
+            //foreach (var state in s.GenerateChildStates())
+            //{
+            //    if (state.Parent != s)
+            //        isSameParent = false;
+            //    Console.WriteLine("\nPARENT\n");
+            //    state.Parent.Traffic.PrintGrid();
+            //    Console.WriteLine("\nchild\n");
+            //    state.Traffic.PrintGrid();
+            //}
 
-            //var g2 = g.Move(g.Cars.Find(c => c.Color == 'r'), Direction.Right, 2);
-
-            //System.Console.WriteLine("\nParent \n");
-            //g.PrintGrid();
-
-            //System.Console.WriteLine("\n Child \n");
-            //g2.PrintGrid();
-
-            //State s = new State();
-            //s.Traffic = g;
-            //s.Parent = null;
-
-            //if (s.IsFinal())
-            //    Console.WriteLine("this state is final");
-            //else
-            //    Console.WriteLine("not final");
+            //if (isSameParent)
+            //    Console.WriteLine("\n the paret works");
 
             System.Console.ReadLine();
-
-           
         }
+
+        
     }
 }
--- RushHour/Solver.cs
+++ RushHour/Solver.cs
@@ -8,5 +8,37 @@ namespace RushHour
 {
     class Solver
     {
+        public void BFSolve(State entry)
+        {
+            Queue<State> stateQueue = new Queue<State>();
+            //if(entry.Parent!=null)
+            //    throw new Exception()
+            stateQueue.Enqueue(entry);
+
+            while (stateQueue.Count > 0)
+            {
+                State tested = stateQueue.Dequeue();
+                if (tested.IsFinal())
+                {
+                    System.Console.WriteLine("WE found a solution! YES! it goes like this");
+                    State.PrintRoute(tested);
+                    return;
+                }
+                else
+                {
+                    foreach (var state in tested.GenerateChildStates())
+                    {
+                        stateQueue.Enqueue(state);
+                    }
+                }
+            }
+
+            System.Console.WriteLine("Dang...unsolvable");
+        }
+
+        public void DFSolve(State entry)
+        {
+
+        }
     }
 }
