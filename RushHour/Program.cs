7a9bbd2caf0b59508e80fd7a0b744286d718303c
raethlo <roman.rostar@gmail.com>
2014-03-23 22:06:09 +0100

    adds menu

--- RushHour/Program.cs
+++ RushHour/Program.cs
@@ -15,29 +15,56 @@ namespace RushHour
             State entry = new State();
             entry.Traffic = new TrafficGrid();
 
+            Console.WriteLine("Which board should i load? (1,2,3,4,5)");
+            var response = Console.ReadLine();
+            int board = -1;
+            try
+            {
+                board = int.Parse(response);
+                if (board > 5 || board < 0)
+                    throw new Exception();
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("invalid board number");
+                return;
+            }
+
+            //nacitanie boardu
             try 
-	        {	        
-		        entry.Traffic.LoadGridFromFile("../../Resources/1.txt");
+	        {	     
+                
+		        entry.Traffic.LoadGridFromFile(String.Format("../../Resources/{0}.txt",board));
 	        }
 	        catch (Exception ex)
 	        {
 		        System.Console.WriteLine(ex.Message);
                 return;
-		        //throw;
 	        }
 
             entry.Parent = null;
-            Console.WriteLine("STARTING FROM");
-            entry.Traffic.PrintGrid();
-            Console.WriteLine("_______________________________\n");
+            Console.WriteLine("Breadth first ('B') or depth first (D)?");
             Solver solver = new Solver();
-            solver.BFSolve(entry);
-            solver.BFSolve(entry, "./output.txt");
-
-            //if (isSameParent)
-            //    Console.WriteLine("\n the paret works");
-           // System.Console.WriteLine( "Traffiiiiic \n" + entry.Traffic.ToString());
-           
+            switch (Console.ReadLine().ToUpper())
+	        {
+                case "D":
+                    Console.WriteLine("STARTING FROM");
+                    Console.WriteLine(entry.Traffic.PrintGrid());
+                    Console.WriteLine("_______________________________\n");                   
+                    solver.DFSolve(entry);
+                    solver.DFSolve(entry, "./output.txt");
+                    break;
+                case "B":
+                    Console.WriteLine("STARTING FROM");
+                    Console.WriteLine(entry.Traffic.PrintGrid());
+                    Console.WriteLine("_______________________________\n");
+                    solver.BFSolve(entry);
+                    solver.BFSolve(entry, "./output.txt");
+                    break;
+		        default:
+                    break;
+	        }
 
             System.Console.ReadLine();
 
--- RushHour/Solver.cs
+++ RushHour/Solver.cs
@@ -110,5 +110,39 @@ namespace RushHour
 
             System.Console.WriteLine("Dang...unsolvable");
         }
+
+        public void DFSolve(State entry,string path)
+        {
+            Stack<State> stateStack = new Stack<State>();
+            Dictionary<string, State> setOfStates = new Dictionary<string, State>();
+            //if(entry.Parent!=null)
+            //    throw new Exception()
+            stateStack.Push(entry);
+
+            while (stateStack.Count > 0)
+            {
+                State tested = stateStack.Pop();
+                if (tested.IsFinal())
+                {
+                    System.Console.WriteLine("WE found a solution! YES! look to the otput file for more info");
+                    State.PrintReverseRouteToFile(tested, path);
+                    return;
+                }
+                else
+                {
+                    foreach (var state in tested.GenerateChildStates())
+                    {
+                        if (!setOfStates.ContainsKey(state.Traffic.ToString()))
+                        {
+                            stateStack.Push(state);
+                            setOfStates.Add(state.Traffic.ToString(), state);
+                        }
+
+                    }
+                }
+            }
+
+            System.Console.WriteLine("Dang...unsolvable");
+        }
     }
 }
